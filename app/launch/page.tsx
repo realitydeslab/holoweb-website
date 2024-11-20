ca5c46e3f39ed78d862824a9b7ac31e275b227d7
Botao Hu <me@botao.hu>
2024-11-20 05:24:46 -0500

    fix

---
+++ app/launch/page.tsx
@@ -0,0 +1,6 @@
+import Link from "next/link";
+
+export default function Launch() {
+  return (<></>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
 
 export default function Home() {
+
   return (
     <div className="container mx-auto px-4">
-      <Link href="https://holoweb.app/launch?url=https://toji.github.io/webxr-particles/">Toji Example</Link>
+      <Link href="launch?url=https://toji.github.io/webxr-particles/">Toji Example</Link>
     </div>
   );
 }
