927c7a2bdd6e45b3e9638e8ff47ea7bda2112481
Botao Hu <me@botao.hu>
2024-11-20 05:30:15 -0500

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,8 @@ export default function Home() {
 
   return (
     <div className="container mx-auto px-4">
-      <Link href="launch?url=https://toji.github.io/webxr-particles/">Toji Example</Link>
+      <h1><Link href="launch?url=https://toji.github.io/webxr-particles/">Toji Example</Link></h1>
+      <h1><Link href="launch?url=https://immersive-web.github.io/webxr-samples/immersive-ar-session.html">immersive-web Example</Link></h1>
     </div>
   );
 }
